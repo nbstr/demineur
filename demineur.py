fff7ec99acad4f6ad2bd9821a24b0ab5073f2e48
nbstr <nab.kml@gmail.com>
2014-11-23 18:20:18 +0100

    fonction main()

--- demineur.py
+++ demineur.py
@@ -7,12 +7,18 @@ from functions import *
 #=========================================#
 # MAIN                                    #
 #=========================================#
+def main():
+    """
+    Application principale
+    """
+    # TITRE BEAUTIFUL
+    header()
+    # INITIALISER CHAMP DE MINES
+    champ = init_champ()
+    # AFFICHER CHAMP DE MINES
+    print_champ(champ)
 
-header("ADZDEVIL")
-# INITIALISER CHAMP DE MINES
-champ = init_champ()
-# AFFICHER CHAMP DE MINES
-print_champ(champ)
+main()
 
 
 
