563e25d93ac2e3f2250b1e210e39536861baee7e
nabster <nab.kml@gmail.com>
2014-11-23 18:11:34 +0100

    reorganisation

--- demineur.py
+++ demineur.py
@@ -31,51 +31,6 @@ def header(text="DÉMINEUR", vp=1, p=6, m=7, s="#", s2="="):
     print(margin + s + s2*(len(text) + 2*p) + s + "\n")
 
 header()
-    
-#=========================================#
-# GRILLE                                  #
-#=========================================#   
-
-# BOMBES
-grille = {}
-grille [(0 ,7)] = "B"
-grille [(1 ,5)] = "B"
-grille [(1 ,6)] = "B"
-grille [(1 ,8)] = "B"
-grille [(2 ,4)] = "B"
-grille [(3 ,4)] = "B"
-grille [(5 ,5)] = "B"
-grille [(5 ,7)] = "B"
-grille [(7 ,0)] = "B"
-grille [(7 ,5)] = "B"
-
-def init_champ():
-    """
-    Initialise le champ de mines.
-    """
-    champ = []
-    for i in range(9):
-        champ.append(["*"] * 9)
-
-    # for bombe in grille:
-    #     print ("x: {}, y: {}, valeur: {}".format(bombe[0], bombe[1], grille[bombe]))
-    #     plateau[bombe[1]][bombe[0]] = grille[bombe]
-
-    return champ
-
-def print_champ(g):
-    """
-    Affiche le champ de mines.
-    """
-    for ligne in g:
-        print ("| " + " | ".join(ligne) + " |\n")
-    print ("\n")
-
-def bombe(coordonnees):
-    """
-    Vérifie s'il y a une bombe aux coordonnées indiquées.
-    """
-    return bombe == coordonnees;
 
 # INITIALISER CHAMP DE MINES
 champ = init_champ()
--- functions.py
+++ functions.py
@@ -1,3 +1,54 @@
 #=========================================#
+# CONFIG                                  #
+#=========================================#  
+
+# BOMBES
+grille = {}
+grille [(0 ,7)] = "B"
+grille [(1 ,5)] = "B"
+grille [(1 ,6)] = "B"
+grille [(1 ,8)] = "B"
+grille [(2 ,4)] = "B"
+grille [(3 ,4)] = "B"
+grille [(5 ,5)] = "B"
+grille [(5 ,7)] = "B"
+grille [(7 ,0)] = "B"
+grille [(7 ,5)] = "B"
+
+# DIFFICULTÉ
+difficulte = 9
+
+#=========================================#
 # FUNCTIONS                               #
 #=========================================#
+
+def init_champ():
+    """
+    Initialise le champ de mines.
+    """
+    champ = []
+    for i in range(difficulte):
+        champ.append(["*"] * difficulte)
+
+    # for bombe in grille:
+    #     print ("x: {}, y: {}, valeur: {}".format(bombe[0], bombe[1], grille[bombe]))
+    #     plateau[bombe[1]][bombe[0]] = grille[bombe]
+
+    return champ
+
+def print_champ(g):
+    """
+    Affiche le champ de mines.
+    """
+    for ligne in g:
+        print ("| " + " | ".join(ligne) + " |\n")
+    print ("\n")
+
+def bombe(x, y):
+    """
+    Vérifie s'il y a une bombe aux coordonnées indiquées.
+    """
+    for bombe in grille:
+        if (bombe == (x, y)):
+            return True;
+    return False;
