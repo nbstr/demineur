a636d8770df8402f484cc7e1f6bb8e4feea8bfb8
nbstr <nab.kml@gmail.com>
2014-11-23 18:22:44 +0100

    notes

---
+++ notes.py
@@ -0,0 +1,5 @@
+## parcourir le dict des bombes
+
+# for bombe in grille:
+#     print ("x: {}, y: {}, valeur: {}".format(bombe[0], bombe[1], grille[bombe]))
+#     plateau[bombe[1]][bombe[0]] = grille[bombe]
