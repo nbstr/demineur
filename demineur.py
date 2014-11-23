6a23aa367debce1f1fc2f5c7033ad918cd2066ac
nabster <nab.kml@gmail.com>
2014-11-23 18:14:49 +0100

    titre adzdevil

--- demineur.py
+++ demineur.py
@@ -1,37 +1,14 @@
 #=========================================#
 # IMPORTS                                 #
 #=========================================#
-
 from beautiful import *
 from functions import *
 
 #=========================================#
-# HEADER                                  #
+# MAIN                                    #
 #=========================================#
 
-def header(text="DÉMINEUR", vp=1, p=6, m=7, s="#", s2="="):
-    """
-    Affiche un beau titre.
-    """
-    padding = p*" "
-    margin = m*" "
-
-    void = margin + s + " "*(len(text) + 2*p) + s
-
-    print("\n" + margin + s + s2*(len(text) + 2*p) + s)
-
-    for i in range(vp):
-        print(void)
-
-    print(margin + s + padding + text + padding + s)
-
-    for i in range(vp):
-        print(void)
-
-    print(margin + s + s2*(len(text) + 2*p) + s + "\n")
-
-header()
-
+header("ADZDEVIL")
 # INITIALISER CHAMP DE MINES
 champ = init_champ()
 # AFFICHER CHAMP DE MINES
